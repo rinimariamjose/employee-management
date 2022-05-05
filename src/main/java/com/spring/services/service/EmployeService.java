2c6564c250ee5b757f191aee068e211d3c15c3c1
rini <rinimariam94@gmail.com>
2022-05-05 17:53:09 +0530

    commit

--- src/main/java/com/spring/services/service/EmployeService.java
+++ src/main/java/com/spring/services/service/EmployeService.java
@@ -1,6 +1,7 @@
 package com.spring.services.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.spring.services.entites.Employee;
 import com.spring.services.repositories.EmployeRepository;
@@ -11,10 +12,32 @@ import org.springframework.stereotype.Service;
 public class EmployeService {
     @Autowired
     private EmployeRepository employeRepository;
+    
+    //getAllEmployee method
     public List<Employee> getAllEmployees(){
         return employeRepository.findAll();
     }
+    
+    //createEmployee Method
     public Employee createEmploye(Employee employee){
         return employeRepository.save(employee);
     }
+
+    //getEmployeeById
+    public Optional<Employee> getEmployeById(Long id){
+    Optional<Employee> employee = employeRepository.findById(id);
+    return employee;
+    }
+    //updateEmployeById
+    public Employee updateEmployeById(Long id,Employee employee){
+        employee.setId(id);
+        return employeRepository.save(employee);
+    }
+    //deleteEmployeById
+    public void deleteEmployeById(Long id){
+        if(employeRepository.findById(id).isPresent()){
+            employeRepository.deleteById(id);
+        }
+    }
+
 }
